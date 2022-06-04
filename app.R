2251dcead07cfc774585e925d7618772fe9ee2fb
Nan Xiao <me@nanx.me>
2022-06-03 20:56:03 -0400

    Use syntax highlighting for R code output

--- app.R
+++ app.R
@@ -12,13 +12,14 @@ library(rhandsontable)
 
 source("modules/navbar-page-custom.R")
 source("modules/heading-panel.R")
-source("modules/hgraph.R")
-source("modules/df2graph.R")
+source("modules/output-rcode.R")
 source("modules/input-matrix-buttons.R")
 source("modules/help-popover.R")
 source("modules/input-file-button.R")
 source("modules/input-text-addon.R")
 source("modules/sanitize-filename.R")
+source("modules/hgraph.R")
+source("modules/df2graph.R")
 
 # UI ---------------------------------------------------------------------------
 

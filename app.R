dde9059f2f468e40a0e09725d7bb0e24c5838ab9
allenzhuaz <allenzhuaz@gmail.com>
2022-06-10 10:50:42 -0400

    remove unused modules

--- app.R
+++ app.R
@@ -16,7 +16,6 @@ source("modules/help-popover.R")
 source("modules/input-file-button.R")
 source("modules/input-text-addon.R")
 source("modules/sanitize-filename.R")
-source("modules/hgraph.R")
 source("modules/df2graph.R")
 source("modules/adaptive-palette.R")
 

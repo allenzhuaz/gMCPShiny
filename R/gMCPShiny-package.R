606049ee2a2dfef53b08a3f6c0037496c7492bb6
Nan Xiao <me@nanx.me>
2022-11-15 00:39:50 -0500

    Add `@importFrom` for dependencies only used in app code but need to appear in Imports to avoid R CMD check note

--- R/gMCPShiny-package.R
+++ R/gMCPShiny-package.R
@@ -1,3 +1,13 @@
+# List of imported functions only depended by app code
+#' @importFrom brew brew
+#' @importFrom gMCPLite gMCP hGraph matrix2graph setWeights
+#' @importFrom gsDesign gsDesign
+#' @importFrom markdown mark
+#' @importFrom ragg agg_png
+#' @importFrom shinyjs useShinyjs delay
+#' @importFrom shinyMatrix matrixInput updateMatrixInput
+#' @importFrom stringi stri_unescape_unicode stri_sort
+
 #' @keywords internal
 "_PACKAGE"
 NULL
